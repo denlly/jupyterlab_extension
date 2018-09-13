b7b63bc539bfb566e720ce5aea8b0a2b5bbe1a64
Denlly <79107345@qq.com>
2018-09-13 17:18:07 +0800

    widget have comic pic

--- src/index.ts
+++ src/index.ts
@@ -19,32 +19,45 @@ import {
     id: 'jupyterlab_xkcd',
     autoStart: true,
     requires: [ICommandPalette],
-    
+
     activate: (app: JupyterLab, palette: ICommandPalette) => {
-      console.log('JupyterLab extension jupyterlab_xkcd is activated!');
-  
-      // Create a single widget
-      let widget: Widget = new Widget();
-      widget.id = 'xkcd-jupyterlab';
-      widget.title.label = 'xkcd.com';
-      widget.title.closable = true;
-  
-      // Add an application command
-      const command: string = 'xkcd:open';
-      app.commands.addCommand(command, {
-        label: 'Random xkcd comic',
-        execute: () => {
-          if (!widget.isAttached) {
-            // Attach the widget to the main work area if it's not there
-            app.shell.addToMainArea(widget);
-          }
-          // Activate the widget
-          app.shell.activateById(widget.id);
-        }
-      });
-  
-      // Add the command to the palette.
-      palette.addItem({command, category: 'Tutorial'});
+        console.log('JupyterLab extension jupyterlab_xkcd is activated!');
+    
+        // Create a single widget
+        let widget: Widget = new Widget();
+        widget.id = 'xkcd-jupyterlab';
+        widget.title.label = 'xkcd.com';
+        widget.title.closable = true;
+        
+        // add a image
+        let img = document.createElement('img');
+        
+        // Fetch info about a random comic
+        fetch('https:////egszlpbmle.execute-api.us-east-1.amazonaws.com/prod').then(response => {
+            return response.json();
+        }).then(data => {
+            img.src = data.img;
+            img.alt = data.title;
+            img.title = data.alt;
+        });
+        widget.node.appendChild(img);
+        
+        // Add an application command
+        const command: string = 'xkcd:open';
+        app.commands.addCommand(command, {
+            label: 'Random xkcd comic',
+            execute: () => {
+            if (!widget.isAttached) {
+                // Attach the widget to the main work area if it's not there
+                app.shell.addToMainArea(widget);
+            }
+            // Activate the widget
+            app.shell.activateById(widget.id);
+            }
+        });
+    
+        // Add the command to the palette.
+        palette.addItem({command, category: 'Tutorial'});
     }
   };
   
